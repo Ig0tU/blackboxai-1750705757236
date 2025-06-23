8a049e87ca925b2c45443a361c122456ae8e9fec
k0880k <124455599+Ig0tU@users.noreply.github.com>
2025-06-23 15:09:31 -0400

    Add SuperClaude/ui/uploads/com_powerhousecms_v1.0.0/site/powerhousecms.php 1750705771552

---
+++ SuperClaude/ui/uploads/com_powerhousecms_v1.0.0/site/powerhousecms.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package     PowerhouseCMS
+ * @subpackage  com_powerhousecms
+ * @copyright   Copyright (C) 2024 PowerhouseCMS. All rights reserved.
+ * @license     GNU General Public License version 2 or later
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\MVC\Controller\BaseController;
+
+// Get an instance of the controller prefixed by PowerhouseCMS
+$controller = BaseController::getInstance('PowerhouseCMS');
+
+// Perform the Request task
+$controller->execute(Factory::getApplication()->input->get('task'));
+
+// Redirect if set by the controller
+$controller->redirect();
