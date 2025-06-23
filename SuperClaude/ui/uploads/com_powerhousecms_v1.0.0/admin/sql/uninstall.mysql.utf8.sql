f6ba6df8a41466d97351c2f8e802274f602ca1d7
k0880k <124455599+Ig0tU@users.noreply.github.com>
2025-06-23 15:09:45 -0400

    Add SuperClaude/ui/uploads/com_powerhousecms_v1.0.0/admin/sql/uninstall.mysql.utf8.sql 1750705785266

---
+++ SuperClaude/ui/uploads/com_powerhousecms_v1.0.0/admin/sql/uninstall.mysql.utf8.sql
@@ -0,0 +1,9 @@
+--
+-- Uninstall SQL for PowerhouseCMS Component
+--
+
+DROP TABLE IF EXISTS `#__powerhousecms_content`;
+DROP TABLE IF EXISTS `#__powerhousecms_media`;
+DROP TABLE IF EXISTS `#__powerhousecms_templates`;
+DROP TABLE IF EXISTS `#__powerhousecms_sync_log`;
+DROP TABLE IF EXISTS `#__powerhousecms_categories`;
